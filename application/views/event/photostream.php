56c298b22db391caa5cc3e29ffb464c3631a86d8
Andrew Draper <andrew.draper@gmail.com>
2012-07-23 09:17:42 -0400

    additional UI on the photostream view

--- application/views/event/photostream.php
+++ application/views/event/photostream.php
@@ -10,7 +10,34 @@
 			<li><a href="#">Albums</a></li>
 			<li><a href="#">Download</a></li>
 			<li><a id="uploadBTN" href="#">Upload Photos</a></li>
-			<li><a href="#">Share</a></li>
+			<li>
+				<a id="event-nav-share" href="#">Share</a>
+				<div id="event-nav-menu-share" class="event-nav-menu">
+					<a class="photo-share-twitter" href="#">Tweet</a> 
+					<a class="photo-share-facebook" href="#">Share</a> 
+					<a class="photo-share-email" href="#">Email</a>
+				</div>
+			</li>
+			<li>
+				<a id="event-nav-privacy" href="#">Privacy</a>
+				<div id="event-nav-menu-privacy" class="event-nav-menu">
+					
+					<p>Select the groups you'd like to be able to see photos that are uploaded.</p>
+					
+					<ul>
+						<li><input type="checkbox" name="privacy-setting" value="1" /> Only You</li>
+						<li><input type="checkbox" name="privacy-setting" value="2" /> Bride/Groom</li>
+						<li><input type="checkbox" name="privacy-setting" value="3" /> Wedding party</li>
+					</ul>
+					<ul>
+						<li><input type="checkbox" name="privacy-setting" value="4" /> Family</li>
+						<li><input type="checkbox" name="privacy-setting" value="5" /> All Guests</li>
+						<li><input type="checkbox" name="privacy-setting" value="6" CHECKED /> Public</li>
+					</ul>
+					<div class="clearit">&nbsp;</div>
+					<input type="button" value="Save" />
+				</div>
+			</li>
 		</ul>
 	</div>
 	
@@ -170,5 +197,150 @@
 	<img src="/assets/img/FPO/event-photo-8.jpg" />
 	Andrew D.	
 </div>
+<div class="photo">
+	<div class="photo-overlay">
+		<ul class="photo-share">
+			<li><a class="photo-share-twitter" href="#">Tweet</a></li>
+			<li><a class="photo-share-facebook" href="#">Share</a></li>
+			<li><a class="photo-share-email" href="#">Email</a></li>
+		</ul>
+		<h3>Add to:</h3>
+		<div class="photo-buttons">
+			<a class="button button-left addto-prints" href="#">Prints</a>
+			<a class="button button-right addto-album" href="#">Album</a>
+		</div>
+		<a class="photo-enlarge" href="/p/123" title="Enlarge">Enlarge</a>
+		<a class="photo-download" href="#" title="Download">Download</a>
+	</div>
+	<img src="/assets/img/FPO/event-photo-1.jpg" />
+	<img class="photo-comment" title="Uncle Bob dancing up a storm on the dance floor." src="/assets/img/icons/comment.png" /> Andrew D.	
+</div>
+<div class="photo">
+	<div class="photo-overlay">
+		<ul class="photo-share">
+			<li><a class="photo-share-twitter" href="#">Tweet</a></li>
+			<li><a class="photo-share-facebook" href="#">Share</a></li>
+			<li><a class="photo-share-email" href="#">Email</a></li>
+		</ul>
+		<h3>Add to:</h3>
+		<div class="photo-buttons">
+			<a class="button button-left addto-prints" href="#">Prints</a>
+			<a class="button button-right addto-album" href="#">Album</a>
+		</div>
+		<a class="photo-enlarge" href="#" title="Enlarge">Enlarge</a>
+		<a class="photo-download" href="#" title="Download">Download</a>
+	</div>
+	<img src="/assets/img/FPO/event-photo-2.jpg" />
+	Andrew D.	
+</div>
+<div class="photo">
+	<div class="photo-overlay">
+		<ul class="photo-share">
+			<li><a class="photo-share-twitter" href="#">Tweet</a></li>
+			<li><a class="photo-share-facebook" href="#">Share</a></li>
+			<li><a class="photo-share-email" href="#">Email</a></li>
+		</ul>
+		<h3>Add to:</h3>
+		<div class="photo-buttons">
+			<a class="button button-left addto-prints" href="#">Prints</a>
+			<a class="button button-right addto-album" href="#">Album</a>
+		</div>
+		<a class="photo-enlarge" href="#" title="Enlarge">Enlarge</a>
+		<a class="photo-download" href="#" title="Download">Download</a>
+	</div>
+	<img src="/assets/img/FPO/event-photo-3.jpg" />
+	Andrew D.	
+</div>
+<div class="photo">
+	<div class="photo-overlay">
+		<ul class="photo-share">
+			<li><a class="photo-share-twitter" href="#">Tweet</a></li>
+			<li><a class="photo-share-facebook" href="#">Share</a></li>
+			<li><a class="photo-share-email" href="#">Email</a></li>
+		</ul>
+		<h3>Add to:</h3>
+		<div class="photo-buttons">
+			<a class="button button-left addto-prints" href="#">Prints</a>
+			<a class="button button-right addto-album" href="#">Album</a>
+		</div>
+		<a class="photo-enlarge" href="#" title="Enlarge">Enlarge</a>
+		<a class="photo-download" href="#" title="Download">Download</a>
+	</div>
+	<img src="/assets/img/FPO/event-photo-4.jpg" />
+	Andrew D.	
+</div>
+<div class="photo">
+	<div class="photo-overlay">
+		<ul class="photo-share">
+			<li><a class="photo-share-twitter" href="#">Tweet</a></li>
+			<li><a class="photo-share-facebook" href="#">Share</a></li>
+			<li><a class="photo-share-email" href="#">Email</a></li>
+		</ul>
+		<h3>Add to:</h3>
+		<div class="photo-buttons">
+			<a class="button button-left addto-prints" href="#">Prints</a>
+			<a class="button button-right addto-album" href="#">Album</a>
+		</div>
+		<a class="photo-enlarge" href="#" title="Enlarge">Enlarge</a>
+		<a class="photo-download" href="#" title="Download">Download</a>
+	</div>
+	<img src="/assets/img/FPO/event-photo-5.jpg" />
+	Andrew D.	
+</div>
+<div class="photo">
+	<div class="photo-overlay">
+		<ul class="photo-share">
+			<li><a class="photo-share-twitter" href="#">Tweet</a></li>
+			<li><a class="photo-share-facebook" href="#">Share</a></li>
+			<li><a class="photo-share-email" href="#">Email</a></li>
+		</ul>
+		<h3>Add to:</h3>
+		<div class="photo-buttons">
+			<a class="button button-left addto-prints" href="#">Prints</a>
+			<a class="button button-right addto-album" href="#">Album</a>
+		</div>
+		<a class="photo-enlarge" href="#" title="Enlarge">Enlarge</a>
+		<a class="photo-download" href="#" title="Download">Download</a>
+	</div>
+	<img src="/assets/img/FPO/event-photo-6.jpg" />
+	Andrew D.	
+</div>
+<div class="photo">
+	<div class="photo-overlay">
+		<ul class="photo-share">
+			<li><a class="photo-share-twitter" href="#">Tweet</a></li>
+			<li><a class="photo-share-facebook" href="#">Share</a></li>
+			<li><a class="photo-share-email" href="#">Email</a></li>
+		</ul>
+		<h3>Add to:</h3>
+		<div class="photo-buttons">
+			<a class="button button-left addto-prints" href="#">Prints</a>
+			<a class="button button-right addto-album" href="#">Album</a>
+		</div>
+		<a class="photo-enlarge" href="#" title="Enlarge">Enlarge</a>
+		<a class="photo-download" href="#" title="Download">Download</a>
+	</div>
+	<img src="/assets/img/FPO/event-photo-7.jpg" />
+	Andrew D.	
+</div>
+<div class="photo">
+	<div class="photo-overlay">
+		<ul class="photo-share">
+			<li><a class="photo-share-twitter" href="#">Tweet</a></li>
+			<li><a class="photo-share-facebook" href="#">Share</a></li>
+			<li><a class="photo-share-email" href="#">Email</a></li>
+		</ul>
+		<h3>Add to:</h3>
+		<div class="photo-buttons">
+			<a class="button button-left addto-prints" href="#">Prints</a>
+			<a class="button button-right addto-album" href="#">Album</a>
+		</div>
+		<a class="photo-enlarge" href="#" title="Enlarge">Enlarge</a>
+		<a class="photo-download" href="#" title="Download">Download</a>
+	</div>
+	<img src="/assets/img/FPO/event-photo-8.jpg" />
+	Andrew D.	
+</div>
+
 
 <div class="clearit">&nbsp;</div>
--- public_html/assets/js/photostream.js
+++ public_html/assets/js/photostream.js
@@ -26,4 +26,38 @@ $(document).ready(function()
 		// store reference of photos id somewhere
 	});
 	
+	// PRIVACY MENU
+	$("#event-nav-privacy").click(function(e) {          
+		e.preventDefault();
+        $("#event-nav-menu-privacy").toggle();
+		$("#event-nav-privacy").toggleClass("menu-open");
+    });
+	
+	$("#event-nav-menu-privacy").mouseup(function() {
+		return false
+	});
+	$(document).mouseup(function(e) {
+		if($(e.target).parent("a#event-nav-privacy").length==0) {
+			$("#event-nav-privacy").removeClass("menu-open");
+			$("#event-nav-menu-privacy").hide();
+		}
+	});
+	
+	// SHARE MENU
+	$("#event-nav-share").click(function(e) {          
+		e.preventDefault();
+        $("#event-nav-menu-share").toggle();
+		$("#event-nav-share").toggleClass("menu-open");
+    });
+	
+	$("#event-nav-menu-share").mouseup(function() {
+		return false
+	});
+	$(document).mouseup(function(e) {
+		if($(e.target).parent("a#event-nav-share").length==0) {
+			$("#event-nav-share").removeClass("menu-open");
+			$("#event-nav-menu-share").hide();
+		}
+	});
+	
 });
