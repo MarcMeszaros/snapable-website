10c936abc79803cacdae1d72a1b00d9d838949df
Marc Meszaros <marc.meszaros@gmail.com>
2012-10-01 16:59:46 -0400

    Fixes a problem with whitespaces and manual guest creation

--- application/models/event_model.php
+++ application/models/event_model.php
@@ -660,6 +660,7 @@ Class Event_model extends CI_Model
 				
 					foreach ( $listArr as $l )
 					{
+						$l = trim($l);
 						if( preg_match("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^", $l) > 0)
 		        	    {
 		        	    	// is email
