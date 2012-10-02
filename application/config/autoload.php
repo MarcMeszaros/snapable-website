2b0f4f3e0d57043eb60985d8bbee2c752fbc7d79
Marc Meszaros <marc.meszaros@gmail.com>
2012-10-02 12:54:38 -0400

    Apparently stuff is case sensitive

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('session', 'snapapi');
+$autoload['libraries'] = array('session', 'SnapApi');
 
 
 /*
