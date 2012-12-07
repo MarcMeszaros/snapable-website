c48877e0e24041a421186149bb754c7ef5e94564
Marc Meszaros <marc.meszaros@gmail.com>
2012-12-07 14:27:46 -0500

    Added raven-php to the website so is logs errors to the sentry server

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('session', 'SnapApi', 'SnapText');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'file');
+$autoload['helper'] = array('url', 'file', 'raven');
 
 
 /*
