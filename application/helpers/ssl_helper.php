ae49f71277988fbf2d6a3d1d2235308dea819a4c
Marc Meszaros <marc.meszaros@gmail.com>
2012-12-08 17:03:31 -0500

    Fixes a bug in the SSL redirect code

--- application/helpers/ssl_helper.php
+++ application/helpers/ssl_helper.php
@@ -9,7 +9,7 @@
         if(!defined('SSL_REDIRECT')) { define('SSL_REDIRECT', true); }
 
         // redirect if required
-        if( (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'on') && SSL_REDIRECT == true)
+        if( (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') && SSL_REDIRECT == true)
         {
             header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
             exit();
