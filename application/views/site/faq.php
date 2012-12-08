e399c29b1f5c7fb6b8c7cdf1f4094cd9f4765833
Marc Meszaros <marc.meszaros@gmail.com>
2012-12-08 15:59:35 -0500

    Updated the FAQ

--- application/views/site/faq.php
+++ application/views/site/faq.php
@@ -1,57 +1,67 @@
 <h1>FAQ</h1>
 
 <h3>What if some of my guests don’t have smartphones?</h3>
-
-<p>If your guests are using a digital camera, they’ll be able to upload the photos after the wedding. </p>
+<p>
+    If your guests are using a digital camera, they’ll be able 
+    to add photos after the wedding. Simply provide the event 
+    link and they can upload photos anytime.
+</p>
 
 <h3>What kind of phones does this work on?</h3>
-
-<p>Currently, <b>Snapable</b> is only available for iPhone. However, stay tuned we’ll be coming up with an Android version soon!</p>
+<p>
+    Currently, <b>Snapable</b> is only available for iPhone. 
+    However, stay tuned we’ll be coming up with an Android version soon!
+</p>
 
 <h3>Can I edit my wedding / album information?</h3>
-
-<p>Yes!</p>
+<p>
+    Yes!
+</p>
 
 <h3>Can I delete a photo?</h3>
-
-<p>You will have complete control over sharing, adding and deleting photos.</p>
+<p>
+    You will have complete control over sharing, adding and deleting photos.
+</p>
 
 <h3>I’m worried this will be complicated for my guests. What do they have to do exactly?</h3>
-
-<p>It’ll be easy for your guests! They simply download the app and your wedding album will be instantly available. At that point, friends and family can snap photos and they’ll be added directly to your album. </p>
+<p>
+    It’ll be easy for your guests! They simply download the app and your wedding album 
+    will be instantly available. At that point, friends and family can snap photos and 
+    they’ll be added directly to your album.
+</p>
 
 <h3>Can I print photos?</h3>
-
-<p>Absolutely! Depending the package you’ve chosen you have from 50 to 500 images inlcluded. </p>
-
-<h3>Can I download my photos?</h3>
-
-<p>Yes.</p>
+<p>
+    Absolutely! Depending the package you’ve chosen you can print on demand, 
+    or up to the set number of prints for that package.
+</p>
 
 <h3>How can I share the album or a specific photos?</h3>
-
-<p>You can share your photo or album on Facebook, Pinterest or through email. </p>
+<p>
+    You can share your photo or album on Facebook, Pinterest or through email.
+</p>
 
 <h3>Can I give anyone access to the album?</h3>
-
-<p>Yes, as the album owner you can keep your album private of your can share it with friends and family. </p>
+<p>
+    Yes, as the album owner you can keep your album private of your can share it with friends and family.
+</p>
 
 <h3>Can my guests order prints?</h3>
-
 <p>Yes, your guests can order their own prints as long as you have provided them access to the album. </p>
 
+<!--
 <h3>What does the slideshow look like?</h3>
-
-<p>Here’s a sample.</p>
+<p>
+    Here’s a sample.
+</p>
 
 <h3>What does the album look like?</h3>
-
-<p>Here’s a sample. </p>
-
-<h3>Can I censor photos from an album?</h3>
-
-<p>As the owner, you have the ability to “hide” select photos from others invited to view the album. </p>
+<p>
+    Here’s a sample.
+</p>
+-->
 
 <h3>How will my photos be shipped?</h3>
-
-<p>We ship regular post. You can expect them within 2-3 weeks of the time of your order.</p>
+<p>
+    We ship regular post. You can expect them within 2-3 weeks of the time of your order.
+</p>
