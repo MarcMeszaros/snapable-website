a2fe9b3993903dc9726e87a6dd662d203a77fb7c
Marc Meszaros <marc.meszaros@gmail.com>
2012-12-14 16:45:25 -0500

    Stole this from the Wedding Republic code

---
+++ application/controllers/troubleshoot.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Troubleshoot extends CI_Controller {
+     
+    public function index()
+    {
+        $head = array(
+            'css' => array(
+                'assets/css/header.css',
+                'assets/css/footer-short.css'
+            ),
+        );
+
+        $this->load->view('common/html_header', $head);
+        $this->load->view('common/header', array('linkHome' => true));
+        $this->load->view('troubleshoot/index');
+        $this->load->view('common/html_footer');
+    }
+}
