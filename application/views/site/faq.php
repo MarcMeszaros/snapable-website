a18e2fceeac4d164eef8af485aff0c958f6f8169
Marc Meszaros <marc.meszaros@gmail.com>
2013-02-21 14:12:01 -0500

    Update FAQ to address 'international transaction fee' questions

--- application/views/site/faq.php
+++ application/views/site/faq.php
@@ -1,5 +1,6 @@
 <h1>FAQ</h1>
 
+<h2>General</h2>
 <h3>What if some of my guests don’t have smartphones?</h3>
 <p>
     If your guests are using a digital camera, they’ll be able to add photos after the wedding. Simply provide the event link and they can upload photos anytime.
@@ -39,4 +40,41 @@
 <h3>Can I give anyone access to the album?</h3>
 <p>Yes, as the album owner you can keep your album private of your can share it with friends and family.</p>
 
+<h2>Billing</h2>
+<h3>Why am I being charged an international transaction fee?</h3>
+<p>
+In some cases, you may see an extra charge on your card called 'international transaction fee', or similar. 
+Some banks and payment cards will add the extra fee on top of the amount we charge, so the total you see is slightly higher than what your receipt will say.
+</p>
+
+<h4>Why is this fee being charged?</h4>
+<p>
+Snapable is based in Canada, and even though we are charging in USD, 
+some banks and card providers will add an additional charge because our bank is not US based.
+It typically appears to be 1-3% of the receipt total that is added on to the total or charged separately to your card.
+</p>
+
+<h4>Who is getting that money?</h4>
+<p>
+The extra fee is going to the bank itself (who may pass it on to the card provider like VISA, for example), 
+and not to Snapable. We are only getting the exact amount we show in your receipt, and we are not able to tell 
+if the additional fee is being taken (otherwise we could note that in the receipt).
+</p>
+
+<h4>Do you also refund the extra charge?</h4>
+<p>
+    Unfortunately we cannot refund the extra charge at this time. We can only refund the amount we charge your card
+    and cannot refund extra charges banks or card providers add to the transaction.
+</p>
+
+<h4>How can I avoid paying it?</h4>
+<p>
+Only a relatively small number of banks seem to add this extra charge, although it does appear to be 
+more common than in previous years. If you have cards with a different bank, or from a different provider, 
+you may find that those cards don't apply an extra cost.
+</p>
+<p>
+At this time, we don't have an alternative payment option that would avoid this issue, but we would like to hear from you if you are affected, 
+so we can fully understand how wide spread the charge is.
+</p>
 
