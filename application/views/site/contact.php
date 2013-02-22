8446c078eb60f8112b125ef898f8cb818684245f
Marc Meszaros <marc.meszaros@gmail.com>
2013-02-22 14:57:32 -0500

    Made the contact form a little more HTML5 friendly

--- application/views/site/contact.php
+++ application/views/site/contact.php
@@ -4,7 +4,7 @@
     <input type="hidden" name="subject" value="Message From Website" />
     <h3>Got a question? We're happy to answer it</h3>
     <label id="contact-email-label">Your Email</label><br/>
-    <input type="text" id="contact-email" name="from" maxlength="255" placeholder="Your email" value="" />
+    <input type="email" id="contact-email" name="from" maxlength="255" placeholder="Your email" value="" />
     <textarea class="message" name="message">Enter a question, comment or message...</textarea>
 
     <!-- The following code is invisible to humans and contains some robot trap text fields. -->
--- public_html/assets/js/site/site.js
+++ public_html/assets/js/site/site.js
@@ -5,14 +5,6 @@ $(document).ready(function(){
         $('#questionForm .message').attr('placeholder', $('#questionForm .message').html());
         $('#questionForm .message').html('');
     }
-    // if we can do the email form
-    if (Modernizr.inputtypes.email) {
-        var oldInput = $('#contact-email');
-        var newInput = $('#contact-email').clone();
-        newInput.attr('type', 'email');
-        newInput.insertBefore(oldInput);
-        oldInput.remove();
-    }
 
     // setup the ajax form
     $('#questionForm').ajaxForm({
