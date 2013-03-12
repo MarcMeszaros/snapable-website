20b5bbda26034bf25ffdf85b8f5cb7916ccb39b9
Marc Meszaros <marc.meszaros@gmail.com>
2013-03-12 18:42:31 -0400

    Add the Vegur font

---
+++ public_html/assets/js/common/default.js
@@ -0,0 +1,7 @@
+// pnotify defaults
+$.pnotify.defaults.history = false;
+$.pnotify.defaults.icon = false;
+$.pnotify.defaults.animation = "slide";
+$.pnotify.defaults.width = "100%";
+$.pnotify.defaults.addclass = "stack-content-center";
+$.pnotify.defaults.stack = {"dir1": "down", "dir2": "right", "push": "bottom", "firstpos1": -1, "firstpos2": -1, "spacing1":0, "spacing2":0};
