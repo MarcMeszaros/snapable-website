8fa17a5821c443af3b19f489793b33290ba5c4fd
Marc Meszaros <marc@snapable.com>
2013-07-14 19:12:19 -0400

    Add missing pinterest icons

--- application/views/photo/index.php
+++ application/views/photo/index.php
@@ -1,3 +1,6 @@
+<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+<script type="text/javascript">stLight.options({publisher: "e0bd9da5-9c45-4ad2-b1a3-81ca7d809ede", doNotHash: true, doNotCopy: true, hashAddressBar: false}); </script>
+
 <div id="photo"><img src="/p/get/<?= $photo_id ?>/480x480" width="480" height="480" alt="Photo" /></div>
 
 <div id="details">
@@ -21,8 +24,7 @@
 	<a class="photo-share-twitter" target="_blank" onclick="_gaq.push(['_trackEvent', 'Share', 'Twitter', 'Event']);" href="http://twitter.com/share?text=<?= urlencode("Follow the photos on at with @getsnapable") ?>&url=https://snapable.com/p/<?= $this->encrypt->encode($photo_id) ?>">Tweet</a> 
 	<a class="photo-share-facebook" target="_blank" onclick="_gaq.push(['_trackEvent', 'Share', 'Facebook', 'Event']);" href="http://www.facebook.com/sharer.php?u=https://snapable.com/p/<?= $this->encrypt->encode($photo_id) ?>">Share</a>
 	<a class="photo-share-pinterest" target="_blank" onclick="_gaq.push(['_trackEvent', 'Share', 'Pinterest', 'Event']);" href="//pinterest.com/pin/create/button/?url=https%3A%2F%2Fsnapable.com%2Fp%2F<?= $this->encrypt->encode($photo_id) ?>&media=<?= urlencode('https://snapable.com/p/get_photo/'.$this->encrypt->encode($photo_id).'/orig') ?>">Pin it</a>
-	<!-- ONLY APPEARS IF IT"S EITHER THE OWNER OF THE GROUP OR TAKER OF THE PHOTO WHO"S VIEWING -->
-	<!--<h2><a id="photo-download" href="#">Download Photo</a></h2>-->
+
 	
 </div>
 
