8647036dab148c21d25da896242613d09208c8c9
Marc Meszaros <marc@snapable.com>
2013-07-22 01:50:10 -0400

    Fix the sharethis buttons.js file so it works with SSL (ie. save a local copy)

--- application/views/common/html_header.php
+++ application/views/common/html_header.php
@@ -76,7 +76,7 @@
         </script>
     <?php } ?>
 
-    <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+    <script type="text/javascript" src="/assets/libs/sharethis/buttons.js"></script>
     <script type="text/javascript">stLight.options({publisher: "e0bd9da5-9c45-4ad2-b1a3-81ca7d809ede", doNotHash: true, doNotCopy: true, hashAddressBar: false});</script>
 
     <?php if (isset($js_vars)) { ?>
