3075077147952dccd761a0fa14b87827a48237c2
Marc Meszaros <marc@snapable.com>
2013-07-23 15:26:49 -0400

    Add new spinner library

--- application/views/common/html_header.php
+++ application/views/common/html_header.php
@@ -47,6 +47,7 @@
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/parsley.js/1.1.10/parsley.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/spin.js/1.2.7/spin.min.js"></script>
     <script type="text/javascript" src="/assets/libs/pnotify/jquery.pnotify.min.js"></script>
     <script type="text/javascript" src="/assets/js/common/default.js"></script>
     <?php
--- public_html/assets/js/common/default.js
+++ public_html/assets/js/common/default.js
@@ -7,3 +7,38 @@ $.pnotify.defaults.addclass = "stack-content-center";
 $.pnotify.defaults.sticker = false;
 $.pnotify.defaults.closer = false;
 $.pnotify.defaults.stack = {"dir1": "down", "dir2": "right", "push": "bottom", "firstpos1": -1, "firstpos2": -1, "spacing1":0, "spacing2":0};
+
+// init spinner
+$(document).ready(function(){
+    // defaults
+    var spinner_defaults = {
+      lines: 13, // The number of lines to draw
+      length: 3, // The length of each line
+      width: 2, // The line thickness
+      radius: 3, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#000', // #rgb or #rrggbb
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: 'auto', // Top position relative to parent in px
+      left: 'auto' // Left position relative to parent in px
+    };
+
+    // add the spinner
+    $('span.spinner-wrap').each(function(){
+        var options = $(this).data(); // get data params
+        var settings = $.extend({}, spinner_defaults, options); // override defaults
+        // calculate the wrap dimensions
+        var box_size = (settings.length*2) + (settings.radius*2) + 4; // add a 4px padding
+        $(this).css('width', box_size+'px');
+        $(this).css('height', box_size+'px');
+        // init the spinner
+        new Spinner(settings).spin($(this).get(0));
+    });
+});
