657544f17fceee9911ab57a96af5e655dc059802
Marc Meszaros <marc@snapable.com>
2013-08-06 18:57:32 -0400

    Remove authentication from PDF

--- application/controllers/pdf.php
+++ application/controllers/pdf.php
@@ -18,8 +18,7 @@ class Pdf extends CI_Controller {
 
     public function download($url=null) 
     {
-        $session_owner = SnapAuth::is_logged_in();
-        if (isset($url) && $session_owner) {
+        if (isset($url)) {
             // get event details
             $verb = 'GET';
             $path = '/event/';
@@ -29,27 +28,14 @@ class Pdf extends CI_Controller {
             $event_resp = SnapApi::send($verb, $path, $params);
             $event_result = json_decode($event_resp['response']);
 
-            // get accounts the user belongs to
-            $sessionIdParts = explode('/', $session_owner['user_uri']);
-            $verb = 'GET';
-            $path = 'user/'.$sessionIdParts[3];
-            $user_resp = SnapApi::send($verb, $path);
-            $user_result = json_decode($user_resp['response']);
-
-            // make sure the the user belongs to the event account, and then delete
-            if (in_array($event_result->objects[0]->account, $user_result->accounts) == true) {
-                // Create new pdf
-                $pdf = new SnapPdf();
-                
-                // fill in the data for the PDF
-                $pdf->title($event_result->objects[0]->title);
-                $pdf->url($event_result->objects[0]->url);
-                $pdf->pin($event_result->objects[0]->pin);
-                $pdf->Output($event_result->objects[0]->url . '.pdf', 'D');
-            } else {
-                $this->output->set_status_header('404');
-            }
-
+            // Create new pdf
+            $pdf = new SnapPdf();
+            
+            // fill in the data for the PDF
+            $pdf->title($event_result->objects[0]->title);
+            $pdf->url($event_result->objects[0]->url);
+            $pdf->pin($event_result->objects[0]->pin);
+            $pdf->Output($event_result->objects[0]->url . '.pdf', 'D');
         } else {
             $this->output->set_status_header('404');
         }
