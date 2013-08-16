5b2f68183d4b0f880b470546ab43a732ea9e5929
Marc Meszaros <marc@snapable.com>
2013-08-16 09:58:26 -0400

    Add Facebook pixel tracking

--- application/views/common/html_header.php
+++ application/views/common/html_header.php
@@ -112,6 +112,23 @@
  <?php } ?>
 	</script>
 
+    <?php if ( $_SERVER['HTTP_HOST'] == "snapable.com" || $_SERVER['HTTP_HOST'] == "www.snapable.com" ) { ?>
+    <script type="text/javascript">
+    var fb_param = {};
+    fb_param.pixel_id = '6008243038461';
+    fb_param.value = '0.00';
+    fb_param.currency = 'USD';
+
+    (function(){
+      var fpw = document.createElement('script');
+      fpw.async = true;
+      fpw.src = '//connect.facebook.net/en_US/fp.js';
+      var ref = document.getElementsByTagName('script')[0];
+      ref.parentNode.insertBefore(fpw, ref);
+    })();
+    </script>
+    <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/offsite_event.php?id=6008243038461&amp;value=0&amp;currency=USD" /></noscript>
+    <?php } ?>
 </head>
 
 <body id="top">
