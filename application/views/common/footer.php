d2b2586689588f3afc23fd05d8b9242c4cc43629
Marc Meszaros <marc@snapable.com>
2013-09-04 23:03:55 -0400

    Fix the photo stream to use bootstrap layouts

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -1,13 +1,11 @@
-	<div class="container">
-		<div class="col-lg-10 col-lg-push-1">
-		<section id="footer">
-	    	&copy; <?= date("Y") ?> Snapable / <a href="/site/contact">Contact</a> / <a href="/site/faq">FAQ</a> / <a href="http://blog.snapable.com">Blog</a> / <a href="/site/terms">Terms of Service</a> / <a href="/site/privacy">Privacy</a>
-	    	
-	    	<div id="sm-links">
-	    		<a id="sm-twitter" href="http://twitter.com/getsnapable" target="_blank">Follow us</a>
-	    		<a id="sm-facebook" href="http://facebook.com/snapable" target="_blank">Like us</a>
-	    		<a id="sm-pinterest" href="http://pinterest.com/snapable" target="_blank">Follow us</a>
-	    	</div>
-	    </section>
-		</div>
+	<div class="col-lg-10 col-lg-push-1" style="padding-left:0;">
+	<section id="footer">
+    	&copy; <?= date("Y") ?> Snapable / <a href="/site/contact">Contact</a> / <a href="/site/faq">FAQ</a> / <a href="http://blog.snapable.com">Blog</a> / <a href="/site/terms">Terms of Service</a> / <a href="/site/privacy">Privacy</a>
+    	
+    	<div id="sm-links">
+    		<a id="sm-twitter" href="http://twitter.com/getsnapable" target="_blank">Follow us</a>
+    		<a id="sm-facebook" href="http://facebook.com/snapable" target="_blank">Like us</a>
+    		<a id="sm-pinterest" href="http://pinterest.com/snapable" target="_blank">Follow us</a>
+    	</div>
+    </section>
 	</div>
