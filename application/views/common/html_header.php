77aab66bb688b7dd7742c5e01da6921a1c30e189
Marc Meszaros <marc@snapable.com>
2013-10-29 22:12:17 -0400

    Add new analytics code

--- application/views/common/html_header.php
+++ application/views/common/html_header.php
@@ -99,20 +99,29 @@
     <![endif]-->
 
     <script type="text/javascript">
-<?php if ( $_SERVER['HTTP_HOST'] == "snapable.com" || $_SERVER['HTTP_HOST'] == "www.snapable.com" ) { ?>  
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-295382-36']);
-	  _gaq.push(['_setDomainName', 'snapable.com']);
-	  _gaq.push(['_trackPageview']);
-	
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
- <?php } else { ?> 
- 	var _gaq = _gaq || [];
- <?php } ?>
+    <?php if ( $_SERVER['HTTP_HOST'] == "snapable.com" || $_SERVER['HTTP_HOST'] == "www.snapable.com" ) { ?> 
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-38299813-2', 'snapable.com');
+        ga('send', 'pageview');
+
+        // old analytics
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-295382-36']);
+        _gaq.push(['_setDomainName', 'snapable.com']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    <?php } else { ?> 
+        var _gaq = _gaq || [];
+    <?php } ?>
 	</script>
 
     <?php if (isset($facebook_pixel) && $facebook_pixel == true && ($_SERVER['HTTP_HOST'] == "snapable.com" || $_SERVER['HTTP_HOST'] == "www.snapable.com")) { ?>
