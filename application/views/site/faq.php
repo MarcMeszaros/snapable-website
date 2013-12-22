aab2a2024983728b42a598960e929da36d50ba61
Marc Meszaros <marc@snapable.com>
2013-12-22 17:12:24 -0500

    Update the FAQ page and add table of contents at the top

--- application/views/site/faq.php
+++ application/views/site/faq.php
@@ -1,8 +1,67 @@
 <div class="col-lg-7">
-    <h1>FAQ</h1>
+    <div class="page-header">
+        <h1>FAQ</h1>
+    </div>
 
-    <h2>GENERAL</h2>
-    <h3>What is Snapable?</h3>
+    <!-- Table of Contents -->
+    <ul>
+        <!-- General -->
+        <li>
+            <a href="#general">General</a>
+            <ul>
+                <li><a href="#general_what-is-snapable">What is Snapable?</a></li>
+                <li><a href="#general_smartphones">What if some of my guests don’t have smartphones?</a></li>
+                <li><a href="#general_let-people-know">How can I let people know about Snapable?</a></li>
+                <li><a href="#general_supported-devices">What kind of phones does this work on?</a></li>
+                <li><a href="#general_edit-event">Can I edit my event / album information?</a></li>
+                <li><a href="#general_delete-photos">Can I delete a photo?</a></li>
+                <li><a href="#general_moderate-event">Can I moderate the album as guests add photos?</a></li>
+                <li><a href="#general_livestream">Can I live stream my event?</a></li>
+                <li><a href="#general_complicated">I’m worried this will be complicated for my guests.</a></li>
+                <li><a href="#general_download">Can I download my photos?</a></li>
+                <li><a href="#general_sharing">Can I share the album or a specific photos?</a></li>
+            </ul>
+        </li>
+
+        <!-- Photos -->
+        <li>
+            <a href="#photos">Photos</a>
+            <ul>
+                <li><a href="#photos_upload">How do we post pictures?</a></li>
+                <li><a href="#photos_upload-limit">Is there a size limit for the photos uploaded through the site?</a></li>
+                <li><a href="#photos_date-usage">How much data will Snapable use while guests take photos at the event?</a></li>
+                <li><a href="#photos_originals">It seems like all the photos are cropped? Can I get the originals?</a></li>
+            </ul>
+        </li>
+
+        <!-- Privacy/Security -->
+        <li>
+            <a href="#privacy-and-security">Privacy &amp; Security</a>
+            <ul>
+                <li><a href="#privacy-and-security_private-event">Can I keep my album private?</a></li>
+                <li><a href="#privacy-and-security_event-address">Why are you asking for the full event address?</a></li>
+                <li><a href="#privacy-and-security_search">Why can't I find my event in the "Nearby Event" list or by search?</a></li>
+            </ul>
+        </li>
+
+        <!-- Billing -->
+        <li>
+            <a href="#billing">Billing</a>
+            <ul>
+                <li><a href="#billing_international-fee">Why am I being charged an international transaction fee?</a></li>
+                <li><a href="#billing_international-fee-why">Why is this fee being charged?</a></li>
+                <li><a href="#billing_international-fee-who">Who is getting that money?</a></li>
+                <li><a href="#billing_international-fee-refund">Do you also refund the extra charge?</a></li>
+                <li><a href="#billing_international-fee-avoid">How can I avoid paying it?</a></li>
+            </ul>
+        </li>
+    </ul>
+
+    <hr class="dotted" />
+
+    <!-- General -->
+    <h2><a id="general"></a>GENERAL</h2>
+    <h3><a id="general_what-is-snapable"></a>What is Snapable?</h3>
     <p>
         Snapable is a photo aggregator that allows event organizers to capture every photo that guests take at the event. 
         It’s the easiest way to collect all of the photos into one online album for sharing and download. It’s instant and 
@@ -10,13 +69,13 @@
         would have otherwise captured.
     </p>
 
-    <h3>What if some of my guests don’t have smartphones?</h3>
+    <h3><a id="general_smartphones"></a>What if some of my guests don’t have smartphones?</h3>
     <p>
         If your guests are using a digital camera, they’ll be able to add photos after the event. Simply provide the event 
         link and, if your event is set as private the PIN code, and they can upload photos anytime.
     </p>
 
-    <h3>How can I let people know about Snapable?</h3>
+    <h3><a id="general_let-people-know"></a>How can I let people know about Snapable?</h3>
     <p>There are a few ways you can let your guests know to use Snapable on the day of the event.</p>
     <ol>
         <li>Invite guests to view your album online and download the app from your Snapable dashboard.</li>
@@ -24,53 +83,59 @@
         <li>Have the MC make an announcement letting guests know they can use the app to take photos.</li>
     </ol>
 
-    <h3>What kind of phones does this work on?</h3>
+    <h3><a id="general_supported-devices"></a>What kind of phones does this work on?</h3>
     <p>
         Snapable is available for iPhone and Android.
     </p>
 
-    <h3>Can I edit my event / album information?</h3>
+    <h3><a id="general_edit-event"></a>Can I edit my event / album information?</h3>
     <p>
         Yes! You can edit the date, time, title and cover image of your event anytime.
     </p>
 
-    <h3>Can I delete a photo?</h3>
+    <h3><a id="general_delete-photos"></a>Can I delete a photo?</h3>
     <p>
         As the account owner, you will have complete control over sharing, adding and deleting photos.
     </p>
 
-    <h3>Can I moderate the album as guests add photos?</h3>
+    <h3><a id="general_moderate-event"></a>Can I moderate the album as guests add photos?</h3>
     <p>
-        Currently, you cannot moderate your album on the fly, however we are working on an update to Snapable and hope to 
-        provide that feature shortly!    
+        You can moderate your album on the fly in one of two ways. You can set your event to automatically allow all uploaded photos 
+        and disable individual photos, or you can set your event to require manual approval of all uploaded photos before they show up
+        in the photo stream.    
     </p>
 
-    <h3>Can I live stream my event?</h3>
+    <h3><a id="general_livestream"></a>Can I live stream my event?</h3>
     <p>
-        Live streaming events will be available within the next few months. Please contact us with the date of your event 
-        and we can provide an update on this feature!
+        You can live stream images from the website as they are uploaded using the <em>Live Slideshow</em> feature on the
+        event page.
+    </p>
+    <p>
+        <strong>Live streaming events is available as a <em>beta</em> feature for event organizers. If you find any issues, 
+        <a href="/site/contact">let us know</a>!</strong>
     </p>
 
-    <h3>I’m worried this will be complicated for my guests. What do they have to do exactly?</h3>
+    <h3><a id="general_complicated"></a>I’m worried this will be complicated for my guests. What do they have to do exactly?</h3>
     <p>
         It’ll be easy for your guests! They simply download the app and find your event. They can search for your event by 
         name or event link, but on the day of the event, your album will be instantly available to them. Then friends and 
         family can snap photos and they’ll be automatically added directly to your album.
     </p>
 
-    <h3>Can I download my photos?</h3>
+    <h3><a id="general_download"></a>Can I download my photos?</h3>
     <p>
-        Absolutely! Download your photos anytime
+        Absolutely! Download your photos anytime.
     </p>
 
-    <h3>Can I share the album or a specific photos?</h3>
+    <h3><a id="general_sharing"></a>Can I share the album or a specific photos?</h3>
     <p>
         You can share an individual photo or the entire album on Facebook, Pinterest or Twitter. You can also download 
         individual photos and send them privately via email.
     </p>
 
-    <h2>ADDING PHOTOS TO YOUR SNAPABLE ALBUM<h2>
-    <h3>How do we post pictures?</h3>
+    <!-- Photos -->
+    <h2><a id="photos"></a>ADDING PHOTOS TO YOUR SNAPABLE ALBUM<h2>
+    <h3><a id="photos_upload"></a>How do we post pictures?</h3>
     <p>There are currently three ways to post pictures to an event, but we’re not stopping there.</p>
     <ol>
         <li>You can upload photos online directly from your event photo album. Just click on the Add Photo button!</li>
@@ -79,14 +144,14 @@
 
     <i>Coming soon! Import photos from other sites and apps including Instagram, Facebook, Flickr and Picassa!</i>
 
-    <h3>Is there a size limit for the photos uploaded through the site?</h3>
+    <h3><a id="photos_upload-limit"></a>Is there a size limit for the photos uploaded through the site?</h3>
     <p>
         Currently, we allow JPEG images smaller than 10MB in size to be uploaded via the website. Please note that the quality 
         of the images are only as good as the images that are uploaded - This applies to website and mobile app uploads, 
         which may include photos from smartphones with varying degrees of camera quality.
     </p>
 
-    <h3>How much data will Snapable use while guests take photos at the event?</h3>
+    <h3><a id="photos_data-usage"></a>How much data will Snapable use while guests take photos at the event?</h3>
     <p>
         While a wireless connection is ideal, if this is not available guests can use Snapable with minimum impact on their 
         data usage. Please be sure check to see if your event venue either provides wi-fi or is located in an area where guests 
@@ -97,21 +162,22 @@
         could be anywhere from 21 to 50 MB of data usage. But keep in mind that this would equate to between 30 and 70 photos!
     </p>
 
-    <h3>It seems like all the photos are cropped? Can I get the originals?</h3>
+    <h3><a id="photos_originals"></a>It seems like all the photos are cropped? Can I get the originals?</h3>
     <p>
         Yes. The original uncropped versions are available for download when logged in as the event organizer. We use the 
         original photos to create cropped versions when displaying them on the mobile apps and website.
     </p>
 
-    <h2>PRIVACY &amp; SECURITY<h2>
-    <h3>Can I keep my album private?</h3>
+    <!-- Privacy & Security -->
+    <h2><a id="privacy-and-security"></a>PRIVACY &amp; SECURITY<h2>
+    <h3><a id="privacy-and-security_private-event"></a>Can I keep my album private?</h3>
     <p>
         Yes, as the album owner you can keep your album private so it will be accessible via a special PIN code. Please note 
         that anyone who is given the PIN code will be able to access the album and upload their own photos via the Snapable 
         app or online at your album website.
     </p>
 
-    <h3>Why are you asking for the full event address?</h3>
+    <h3><a id="privacy-and-security_event-address"></a>Why are you asking for the full event address?</h3>
     <p>
         We collect the full address of the event in order to enable auto-find on the mobile apps. This means your guests 
         simply download the app and your event will automatically pop up, making it quick and easy for your guests to begin 
@@ -126,48 +192,52 @@
         after the event start and end date, simply use the search field provided in the app.
     </p>
 
-    <h3>Why can't I find my event in the "Nearby Event" list or by search?</h3>
+    <h3><a id="privacy-and-security_search"></a>Why can't I find my event in the "Nearby Event" list or by search?</h3>
     <p>
         For privacy reasons, your event will only show up in the "Nearby List" or mobile app search results if your event is 
         in progress. Don't worry about having an exact start and end time. Your event will still show up a few hours before 
         and after the specified start and end time for guests that arrive early or that leave late.
     </p>
 
-    <h2>BILLING</h2>
-    <h3>Why am I being charged an international transaction fee?</h3>
+    <!-- Billing -->
+    <h2><a id="billing"></a>BILLING</h2>
+    <h3><a id="billing_international-fee"></a>Why am I being charged an international transaction fee?</h3>
     <p>
-    In some cases, you may see an extra charge on your card called 'international transaction fee', or similar. 
-    Some banks and payment cards will add the extra fee on top of the amount we charge, so the total you see is slightly higher than what your receipt will say.
+        In some cases, you may see an extra charge on your card called 'international transaction fee', or similar. 
+        Some banks and payment cards will add the extra fee on top of the amount we charge, so the total you see is 
+        slightly higher than what your receipt will say.
     </p>
 
-    <h3>Why is this fee being charged?</h3>
+    <h3><a id="billing_international-fee-why"></a>Why is this fee being charged?</h3>
     <p>
-    Snapable is based in Canada, and even though we are charging in USD, 
-    some banks and card providers will add an additional charge because our bank is not US based.
-    It typically appears to be 1-3% of the receipt total that is added on to the total or charged separately to your card.
+        Snapable is based in Canada, and even though we are charging in USD, 
+        some banks and card providers will add an additional charge because our bank is not US based.
+        It typically appears to be 1-3% of the receipt total that is added on to the total or charged separately to your card.
     </p>
 
-    <h3>Who is getting that money?</h3>
+    <h3><a id="billing_international-fee-who"></a>Who is getting that money?</h3>
     <p>
-    The extra fee is going to the bank itself (who may pass it on to the card provider like VISA, for example), 
-    and not to Snapable. We are only getting the exact amount we show in your receipt, and we are not able to tell 
-    if the additional fee is being taken (otherwise we could note that in the receipt).
+        The extra fee is going to the bank itself (who may pass it on to the card provider like VISA, for example), 
+        and not to Snapable. We are only getting the exact amount we show in your receipt, and we are not able to tell 
+        if the additional fee is being taken (otherwise we could note that in the receipt).
     </p>
 
-    <h3>Do you also refund the extra charge?</h3>
+    <h3><a id="billing_international-fee-refund"></a>Do you also refund the extra charge?</h3>
     <p>
         Unfortunately we cannot refund the extra charge at this time. We can only refund the amount we charge your card
         and cannot refund extra charges banks or card providers add to the transaction.
     </p>
 
-    <h3>How can I avoid paying it?</h3>
+    <h3><a id="billing_international-fee-avoid"></a>How can I avoid paying it?</h3>
     <p>
-    Only a relatively small number of banks seem to add this extra charge, although it does appear to be 
-    more common than in previous years. If you have cards with a different bank, or from a different provider, 
-    you may find that those cards don't apply an extra cost.
+        Only a relatively small number of banks seem to add this extra charge, although it does appear to be 
+        more common than in previous years. If you have cards with a different bank, or from a different provider, 
+        you may find that those cards don't apply an extra cost.
     </p>
     <p>
-    At this time, we don't have an alternative payment option that would avoid this issue, but we would like to hear from you if you are affected, 
-    so we can fully understand how wide spread the charge is.
+        At this time, we don't have an alternative payment option that would avoid this issue, but 
+        <a href="/site/contact">we would like to hear from you</a> if you are affected, so we can fully understand how 
+        wide spread the charge is.
     </p>
+
 </div>
