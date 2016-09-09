75848c335374601cc3a8e7a729162d1176f32e0a
Marc Meszaros <marc@snapable.com>
2016-09-09 01:21:03 -0700

    Disable signup

--- app/application/views/account/signin.php
+++ app/application/views/account/signin.php
@@ -4,19 +4,19 @@
 		<div class="panel-body">
 		<form role="form" id="signinWrap" name="signin" action="/account/validate" method="post" data-validate="parsley" novalidate>
 			<h1>Sign in to your account</h1>
-			<h2>Don't have an account? <a href="/signup">Sign-up here</a></h2>
-			
+			<!-- <h2>Don't have an account? <a href="/signup">Sign-up here</a></h2> -->
+
 			<hr />
-			
-			<?php 
+
+			<?php
 			if ( $error == true ) {
-				echo "<div id='error'>Incorrect password or email address.</div>";	
-			} 
+				echo "<div id='error'>Incorrect password or email address.</div>";
+			}
 			if ( $reset == true ) {
-				echo "<div id='reset'>Your password was successfully reset.</div>";	
-			} 
+				echo "<div id='reset'>Your password was successfully reset.</div>";
+			}
 			?>
-			
+
 			<div class="form-group">
 				<label for="email">Email Address</label>
 				<input class="form-control text-center" type="email" data-type="email" name="email" required="required" data-required="true" />
@@ -33,7 +33,7 @@
 				<button type="submit" name="submit" class="form-control btn btn-primary">Sign in</button>
 			</div>
 			<a id="forgotPassword" href="/account/reset">Forgot your password?</a>
-			
+
 		</form>
 		</div>
 	</div>
