fd31ea5f41d31d5e9eb054a4c2e21f697f3f583f
Marc Meszaros <marc@snapable.com>
2016-09-15 23:38:39 -0700

    Update the copy

--- app/application/views/home/goodbye.php
+++ app/application/views/home/goodbye.php
@@ -7,12 +7,12 @@
     <div class="row">
         <div class="col-xs-12 col-md-8 col-md-push-2">
             <p>
-              Thank you for your support of Snapable over the years. We've enjoyed working on Snapable 
+              Thank you for your support of Snapable over the years. We've enjoyed working on Snapable
               as much as you’ve enjoyed using it. It’s been an honor serving you.
             </p>
             <p class="text-justify">
               Unfortunately we are closing our doors. The interest in the service was not wide
-              spread enough to the cover the operating costs. You can download your photos until we shutdown
+              spread enough to continue operations. You can download your photos until we shutdown
               on <strong>October 1<sup>st</sup>, 2016</strong>. We will be permanently and securely deleting all data after this date.
               This includes account information, hashed passwords, email addresses, all pictures, and any other information
               that was stored in your account.
